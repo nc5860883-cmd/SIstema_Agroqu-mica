9f5ae77fb32420670660a9a6b5401a941425969c
Nancy Castillo <nc5860883@gmail.com>
2026-08-24 15:22:37 -0300

    Cambio de nombre en apps.py

--- A/Agroquimica/apps.py
+++ A/Agroquimica/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AgroquimicaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'agroquimicaa'
+    name = 'agroquimica'
